41a12c37aa202f57fb43fb716e304d8b901e621b
Tian Xia <txia0619@gmail.com>
2020-01-24 10:13:54 -0500

    main.cpp updated

--- CS4500-Assignment-1/main.cpp
+++ CS4500-Assignment-1/main.cpp
@@ -24,6 +24,56 @@ int main(int argv, char** argc) {
     //hashmap_print_file(data_map);
     //data_map->print_self();
     StrList *header_type = get_column_header(max_row, data_map);
+
+    // print out -print_col_type
+    String *print_col_type_idx = cast_string(command_map->get(print_col_type));
+    if (print_col_type_idx) {
+        size_t col = atoi(print_col_type_idx->getValue());
+        size_t type = atoi(header_type->get(col)->getValue());
+        if (type == 1) {
+            std::cout << "STRING" << std::endl;
+        }
+        else if (type == 2) {
+            std::cout << "FLOAT" << std::endl;
+        }
+        else if (type == 3) {
+            std::cout << "INT" << std::endl;
+        }
+        else if (type == 4) {
+            std::cout << "BOOL" << std::endl;
+        }
+        else if (type == 5) {
+            std::cout << "MISSING VALUE" << std::endl;
+        }
+    }
+
+    //print out -is_missing_idx
+    StrList *is_missing_idx_idx = cast_list(command_map->get(is_missing_idx));
+    if (is_missing_idx_idx) {
+        String *column = is_missing_idx_idx->get(0);
+        String *offset = is_missing_idx_idx->get(1);
+        Integer *col = new Integer(atoi(column->getValue()));
+        Integer *off = new Integer(atoi(offset->getValue()));
+        StrList *list = cast_list(data_map->get(off));
+        if (list == nullptr) {
+            std::cout << 1 << std::endl;
+        }
+        else {
+            if (col->val_ >= list->size()) {
+                std::cout << 1 << std::endl;
+            }
+            else {
+                if (strcmp(list->get(col->val_)->getValue(), "<>") != 0) {
+                    std::cout << 0 << std::endl;
+                }
+                else {
+                    std::cout << 1 << std::endl;
+                }
+            }
+        }
+    }
+
+    // print out -print_col_idx
     StrList *idx = cast_list(command_map->get(print_col_idx));
     if (idx) {
         String *column = idx->get(0);
@@ -34,6 +84,9 @@ int main(int argv, char** argc) {
         list->get(col->val_)->print_self();
         printf("\n");
     }
+
+    // print out 
+
 
     return 0;
 }
