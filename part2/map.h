d54f334ba982be54f4cf85168843aac77296395a
tx0619s <xia.tia@husky.neu.edu>
2020-01-24 16:17:23 -0500

    Update map.h

--- part2/map.h
+++ part2/map.h
@@ -25,6 +25,7 @@ class Hashmap : public Object {
         size_t capacity_; 
 
     public:
+        //constructor
         Hashmap() {
             data = new Hashmap_pair*[4];
             //initialize(data);
@@ -32,51 +33,55 @@ class Hashmap : public Object {
             capacity_ = 4;
         }
 
+        // destructor
         ~Hashmap() {
             delete [] data;
         }
 
+        // Initialize hashmap with null pointers
         void initialize(Hashmap_pair **new_data) {
             
         }
 
+        // Return hash index given the key
         size_t hash_index(Object *key) {
             
         }
 
 
-
-        void hashmap_put(Hashmap_pair **temp_data, Object *key, Object *val) {
+        // copy keys and values to a new Hashmap 
+        void copy_to_new(Hashmap_pair **temp_data, Object *key, Object *val) {
             
         }
 
+        // Double the capacity of hashmap when needed
         void expand() {
             
         }
 
-        //get the exact data (not a copy) from Hashmap.
-        //return null if no key inside.
+        // Returns the value to which the specified key is mapped, 
+        // or null if this map contains no mapping for the key.
         Object* get(Object *key) {
             
         }
 
+        // Associates the specified value with the specified key in this map.
         void put(Object *key, Object *val) {
             
         }
 
+        // Removes the mapping for the specified key from this map if present.
         void remove(Object *key) {
             
         }
 
+        // Returns the number of key-value mappings in this map.
         size_t size() {
 
         }
 
+        // Returns a list view of the keys contained in this map.
         Object** key_array() {
-            
-        }
-
-        virtual void print_self() {
             
         }
 };
