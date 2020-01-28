89ad5d30d5f9c2e709c2b6a52ee9a248ce294c14
tx0619s <xia.tia@husky.neu.edu>
2020-01-28 17:27:15 -0500

    Update test-queue.cpp

--- CS4500-Assignment-2/part1/test-queue.cpp
+++ CS4500-Assignment-2/part1/test-queue.cpp
@@ -9,23 +9,23 @@ void t_true(bool p) { if (!p) FAIL(); }
 void t_false(bool p) { if (p) FAIL(); }
 
 //tests strings
-void test1() {
-  String * s = new String("Hello");
-  String * t = new String("World");
-  String * s1 = new String(" Hello ");
-  String * s2 = new String("He");
-  String * u = s->concat(t);
-  t_true(s->equals(s));
-  t_false(s->equals(t));
-  t_false(s->equals(u));
-  t_true(s->compare(s) == 0);
-  t_false(s->compare(t) != 0);
-  t_false(s->equals(s1));
-  t_true(s->equals(s1->trim()));
-  t_true(s->contains(s2));
+// void test1() {
+//   String * s = new String("Hello");
+//   String * t = new String("World");
+//   String * s1 = new String(" Hello ");
+//   String * s2 = new String("He");
+//   String * u = s->concat(t);
+//   t_true(s->equals(s));
+//   t_false(s->equals(t));
+//   t_false(s->equals(u));
+//   t_true(s->compare(s) == 0);
+//   t_false(s->compare(t) != 0);
+//   t_false(s->equals(s1));
+//   t_true(s->equals(s1->trim()));
+//   t_true(s->contains(s2));
   
-  OK("1");
-}
+//   OK("1");
+// }
 
 //tests queues
 void test2() {
@@ -33,7 +33,7 @@ void test2() {
   Queue * b = new Queue();
   Object * s = new String("Hello");
   Object * t = new String("World");
-  Object * s1 = new String(" Hello ");
+  Object * s1 = new String("Hello");
   Object * s2 = new String("He");
   a->enqueue(s);
   a->enqueue(t);
@@ -47,10 +47,11 @@ void test2() {
   t_true((a->size()) == b->size());
   b->dequeue();
   t_false((a->size()) == b->size());
+  OK("OK");
 }
 
 int main() {
-  test1();
+  //test1();
   test2();
   return 0;
 }
