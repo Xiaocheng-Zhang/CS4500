61662d9770d70a37d3c3b41ab352905e3816d707
Xiaocheng-Zhang <zhang.xiaoch@husky.neu.edu>
2020-01-30 16:58:48 -0500

    more info.

--- CS4500-Assignment-2/part1/test-queue.cpp
+++ CS4500-Assignment-2/part1/test-queue.cpp
@@ -1,44 +1,54 @@
-#include "string.h"
 #include "queue.h"
-#include <stdlib.h>
+#include "string.h"
 #include <iostream>
+#include <stdlib.h>
 
-void FAIL() {   exit(1);    }
-void OK(const char* m) { std::cout << m << "\n"; }
-void t_true(bool p) { if (!p) FAIL(); }
-void t_false(bool p) { if (p) FAIL(); }
+void FAIL() { exit(1); }
+void OK(const char *m) { std::cout << m << "\n"; }
+void t_true(bool p) {
+  if (!p)
+    FAIL();
+}
+void t_false(bool p) {
+  if (p)
+    FAIL();
+}
 
-//tests strings
+// tests strings
 void test1() {
-  String * s = new String("Hello");
-  String * t = new String("World");
-  String * s1 = new String(" Hello ");
-  String * s2 = new String("He");
-  String * u = s->concat(t);
+  String *s = new String("Hello");
+  String *t = new String("World");
+  String *s1 = new String(" Hello ");
+  String *s2 = new String("He");
+  String *u = s->concat(t);
   t_true(s->equals(s));
   t_false(s->equals(t));
   t_false(s->equals(u));
-  //t_true(s->compare(s) == 0);
-  //t_false(s->compare(t) != 0);
+  t_true(s->compare(s) == 0);
+  t_true(s->compare(t) != 0);
+  t_false(s->equals(s1));
   s1->trim();
   t_true(s->equals(s1));
-  //t_true(s->contains(s2));
+  t_true(s->contains(s2));
+
   OK("1");
 }
 
-//tests queues
+// tests queues
 void test2() {
-  Queue * a = new Queue();
-  Queue * b = new Queue();
-  Object * s = new String("Hello");
-  Object * t = new String("World");
-  Object * s1 = new String("Hello");
-  Object * s2 = new String("He");
+  Queue *a = new Queue();
+  Queue *b = new Queue();
+  Object *s = new String("Hello");
+  Object *t = new String("World");
+  Object *s1 = new String("Hello");
+  Object *s2 = new String("He");
   a->enqueue(s);
   a->enqueue(t);
+  a->enqueue(s1);
   b->enqueue(s1);
   b->enqueue(s2);
-  
+  b->enqueue(s2);
+
   t_true((a->peek())->equals(b->peek()));
   t_true((a->dequeue())->equals(b->dequeue()));
   t_false((a->dequeue())->equals(b->dequeue()));
@@ -46,11 +56,12 @@ void test2() {
   t_true((a->size()) == b->size());
   b->dequeue();
   t_false((a->size()) == b->size());
-  OK("OK");
 }
 
 int main() {
   test1();
-  //test2();
+  test2();
+  /** puts method help to check tests passed (made by implementor team)*/
+  puts("All tests in test-queue.cpp passed!");
   return 0;
 }
