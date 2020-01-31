2b9bb15e40be3f05e9635952acf5a425abe8451d
Xiaocheng-Zhang <zhang.xiaoch@husky.neu.edu>
2020-01-31 16:29:22 -0500

    new

--- CS4500-Assignment-2/part1/array.h
+++ CS4500-Assignment-2/part1/array.h
@@ -1,37 +1,602 @@
 #pragma once
-#include "object.h"
-#include <stdbool.h>
+#include "array.h"
+#include "string.h"
+#include <assert.h>
+#include <ctype.h>
+#include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/**
- * @brief Represents an array-structured list
- *
- */
 class Array {
 public:
+  Object **list;
   size_t size_;
   size_t capacity_;
   size_t hash_code;
-  void* list;
 
   Array() {
-	  size_ = 0;
-	  capacity_ = 4;
-	  hash_code = 0;
+    size_ = 0;
+    capacity_ = 4;
+    list = new Object *[capacity_];
+    initialize(0);
+    hash_code = 0;
+    hash();
   }
 
-  virtual void clear() {
-	  size_ = 0;
+  ~Array() { delete[] list; }
+
+  virtual void initialize(size_t from) {
+    for (size_t i = from; i < capacity_; i++) {
+      list[i] = nullptr;
+    }
   }
 
-  virtual size_t size() { return size_; }
+  void expand() {
+    capacity_ *= 2;
+    Object **temp = new Object *[capacity_];
+    for (size_t i = 0; i < size_; i++) {
+      temp[i] = list[i];
+    }
+    delete[] list;
+    list = temp;
+    initialize(size_);
+  }
 
-  virtual size_t hash() {
+  void append(Object *e) {
+    if (size_ == capacity_) {
+      expand();
+    }
+    list[size_] = e;
+    size_++;
+  } // Appends e to end
+
+  void add(size_t i, Object *e) {
+    // will there be a space??
+    assert(i <= size_);
+    if (size_ == capacity_) {
+      expand();
+    }
+    if (!list[i]) {
+      list[i] = e;
+      return;
+    }
+    Object **temp = new Object *[capacity_];
+    for (size_t j = 0; j < i; j++) {
+      temp[j] = list[j];
+    }
+    temp[i] = e;
+    for (size_t j = i; j < size_; j++) {
+      temp[j + 1] = list[j];
+    }
+    size_++;
+    delete[] list;
+    list = temp;
+  } // Inserts e at i
+
+  void clear() {
+    delete [] list;
+    list = new Object *[capacity_];
+    initialize(0);
+    size_ = 0;
+  } // Removes all of elements from this list
+
+  bool contains(Object *o) {
+    Object *s = dynamic_cast<Object *>(o);
+    if (!s) {
+      return false;
+    }
+    for (size_t i = 0; i < size_; i++) {
+      if (list[i]->equals(s)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool equals(Object *o) {
+    if (o->hash() == hash_code) {
+      // return true;
+    }
+    Array *l = dynamic_cast<Array *>(o);
+    if (!l) {
+      return false;
+    }
+    if (size_ != l->size()) {
+      return false;
+    }
+	if (hash() == l->hash()) {
+		return true;
+	}
+	return false;
+  } // Compares o with this list for equality.
+
+  Object *get(size_t index) {
+    assert(index < size_ && index >= 0);
+    return list[index];
+  } // Returns the element at index
+
+  size_t hash() {
     if (hash_code == 0) {
       hash_me();
     }
     return hash_code;
   } // Returns the hash code value for this list.
 
-  virtual void hash_me() { hash_code = (size_t)this; }
+  void hash_me() {
+    hash_code = 0;
+    for (size_t i = 0; i < size_; i++) {
+      hash_code += list[i]->hash();
+      // list[i]->print_hash();
+    }
+  }
+
+  virtual Object *remove(size_t i) {
+    assert(i < size_ && i >= 0);
+    Object **temp = new Object *[capacity_];
+    Object *cur;
+    cur = temp[i];
+    for (size_t j = 0; j < i; j++) {
+      // puts(list[j]->getValue());
+      temp[j] = list[j];
+    }
+    for (size_t j = i + 1; j < size_; j++) {
+      // puts(list[j]->getValue());
+      temp[j - 1] = list[j];
+    }
+    delete[] list;
+    list = temp;
+    size_--;
+    return cur;
+  } // Removes the element at i
+
+  size_t size() { return size_; }
+};
+
+/**
+ * @brief Represents an array-structured list
+ *
+ */
+class Int_Array {
+public:
+  int *list;
+  size_t capacity_;
+  size_t size_;
+  size_t hash_code;
+
+  Int_Array() {
+    size_ = 0;
+    capacity_ = 4;
+    hash_code = 0;
+    list = new int[capacity_];
+    hash();
+  }
+
+  void expand() {
+    capacity_ *= 2;
+    int *temp = new int[capacity_];
+    for (size_t i = 0; i < size_; i++) {
+      temp[i] = list[i];
+    }
+    delete[] list;
+    list = temp;
+    // initialize(size_);
+  }
+
+  /**
+   * @brief pushes the given object onto array
+   *
+   * @param o the given object to be added
+   */
+  void append(int i) {
+    if (size_ == capacity_) {
+      expand();
+    }
+    list[size_] = i;
+    size_++;
+  }
+
+  /**
+   * @brief removes all elements from array
+   *
+   */
+  void clear() {
+    delete list;
+    list = new int[capacity_];
+    size_ = 0;
+  }
+
+  /**
+   * @brief checks if the given object is in array
+   *
+   * @param o object to be checked
+   * @return true if object in array
+   * @return false if object not in array
+   */
+  bool contains(int i) {
+    for (size_t m = 0; m < size_; m++) {
+      if (list[m] == i) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * @brief retrieves the object at given position
+   *
+   * @param idx index position
+   * @return Object object located at given index inside array
+   */
+  int get(size_t idx) {
+    assert(idx >= 0 && idx < size_);
+    return list[idx];
+  }
+
+  /**
+   * @brief adds the given object to array at given position
+   *
+   * @param o given object
+   * @param idx index position
+   */
+  void insert(int i, size_t idx) {
+    assert(idx >= 0 && idx <= size_);
+    if (size_ == capacity_) {
+      expand();
+    }
+    if (!list[idx]) {
+      list[idx] = i;
+      return;
+    }
+    int *temp = new int[capacity_];
+    for (size_t j = 0; j < idx; j++) {
+      temp[j] = list[j];
+    }
+    temp[idx] = i;
+    for (size_t j = idx; j < size_; j++) {
+      temp[j + 1] = list[j];
+    }
+    size_++;
+    delete[] list;
+    list = temp;
+  }
+
+  /**
+   * @brief deletes the given object from array
+   *
+   * @param o object to be deleted
+   * @return Object removed object, now unlinked from array
+   */
+  int remove(size_t i) {
+    assert(i < size_ && i >= 0);
+    int *temp = new int[capacity_];
+    int cur;
+    cur = temp[i];
+    for (size_t j = 0; j < i; j++) {
+      temp[j] = list[j];
+    }
+    for (size_t j = i + 1; j < size_; j++) {
+      temp[j - 1] = list[j];
+    }
+    delete[] list;
+    list = temp;
+    size_--;
+    return cur;
+  }
+
+  void hash_me() {
+    hash_code = 0;
+    for (size_t i = 0; i < size_; i++) {
+      hash_code += (list[i] + 17) * 13;
+    }
+  }
+
+  size_t hash() {
+    if (hash_code == 0) {
+      hash_me();
+    }
+    return hash_code;
+  }
+
+  size_t size() { return size_; }
+};
+
+/**
+ * @brief Represents an array-structured list
+ *
+ */
+class Bool_Array {
+public:
+  bool *list;
+  size_t capacity_;
+  size_t size_;
+  size_t hash_code;
+
+  Bool_Array() {
+    size_ = 0;
+    capacity_ = 4;
+    hash_code = 0;
+    list = new bool[capacity_];
+    hash();
+  }
+
+  void expand() {
+    capacity_ *= 2;
+    bool *temp = new bool[capacity_];
+    for (size_t i = 0; i < size_; i++) {
+      temp[i] = list[i];
+    }
+    delete[] list;
+    list = temp;
+    // initialize(size_);
+  }
+
+  /**
+   * @brief pushes the given object onto array
+   *
+   * @param o the given object to be added
+   */
+  void append(bool i) {
+    if (size_ == capacity_) {
+      expand();
+    }
+    list[size_] = i;
+    size_++;
+  }
+
+  /**
+   * @brief removes all elements from array
+   *
+   */
+  void clear() {
+    delete list;
+    list = new bool[capacity_];
+    size_ = 0;
+  }
+
+  /**
+   * @brief checks if the given object is in array
+   *
+   * @param o object to be checked
+   * @return true if object in array
+   * @return false if object not in array
+   */
+  bool contains(bool i) {
+    for (size_t m = 0; m < size_; m++) {
+      if (list[m] == i) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * @brief retrieves the object at given position
+   *
+   * @param idx index position
+   * @return Object object located at given index inside array
+   */
+  bool get(size_t idx) {
+    assert(idx >= 0 && idx < size_);
+    return list[idx];
+  }
+
+  /**
+   * @brief adds the given object to array at given position
+   *
+   * @param o given object
+   * @param idx index position
+   */
+  void insert(bool i, size_t idx) {
+    assert(idx >= 0 && idx <= size_);
+    if (size_ == capacity_) {
+      expand();
+    }
+    if (!list[idx]) {
+      list[idx] = i;
+      return;
+    }
+    bool *temp = new bool[capacity_];
+    for (size_t j = 0; j < idx; j++) {
+      temp[j] = list[j];
+    }
+    temp[idx] = i;
+    for (size_t j = idx; j < size_; j++) {
+      temp[j + 1] = list[j];
+    }
+    size_++;
+    delete[] list;
+    list = temp;
+  }
+
+  /**
+   * @brief deletes the given object from array
+   *
+   * @param o object to be deleted
+   * @return Object removed object, now unlinked from array
+   */
+  bool remove(size_t i) {
+    assert(i < size_ && i >= 0);
+    bool *temp = new bool[capacity_];
+    bool cur;
+    cur = temp[i];
+    for (size_t j = 0; j < i; j++) {
+      // puts(list[j]->getValue());
+      temp[j] = list[j];
+    }
+    for (size_t j = i + 1; j < size_; j++) {
+      // puts(list[j]->getValue());
+      temp[j - 1] = list[j];
+    }
+    delete[] list;
+    list = temp;
+    size_--;
+    return cur;
+  }
+
+  void hash_me() {
+    hash_code = 0;
+    for (size_t i = 0; i < size_; i++) {
+      hash_code += (list[i] + 17) * 13;
+    }
+  }
+
+  size_t size() { return size_; }
+
+  size_t hash() {
+    if (hash_code == 0) {
+      hash_me();
+    }
+    return hash_code;
+  }
+};
+
+/**
+ * @brief Represents an array-structured list
+ *
+ */
+class Float_Array {
+public:
+  float *list;
+  size_t capacity_;
+  size_t size_;
+  size_t hash_code;
+
+  Float_Array() {
+    size_ = 0;
+    capacity_ = 4;
+    hash_code = 0;
+    list = new float[capacity_];
+    hash();
+  }
+
+  void expand() {
+    capacity_ *= 2;
+    float *temp = new float[capacity_];
+    for (size_t i = 0; i < size_; i++) {
+      temp[i] = list[i];
+    }
+    delete[] list;
+    list = temp;
+    // initialize(size_);
+  }
+
+  /**
+   * @brief pushes the given object onto array
+   *
+   * @param o the given object to be added
+   */
+  void append(float i) {
+    if (size_ == capacity_) {
+      expand();
+    }
+    list[size_] = i;
+    size_++;
+  }
+
+  /**
+   * @brief removes all elements from array
+   *
+   */
+  void clear() {
+    delete list;
+    list = new float[capacity_];
+    size_ = 0;
+  }
+
+  /**
+   * @brief checks if the given object is in array
+   *
+   * @param o object to be checked
+   * @return true if object in array
+   * @return false if object not in array
+   */
+  bool contains(float i) {
+    for (size_t m = 0; m < size_; m++) {
+      if (list[m] == i) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * @brief retrieves the object at given position
+   *
+   * @param idx index position
+   * @return Object object located at given index inside array
+   */
+  float get(size_t idx) {
+    assert(idx >= 0 && idx < size_);
+    return list[idx];
+  }
+
+  /**
+   * @brief adds the given object to array at given position
+   *
+   * @param o given object
+   * @param idx index position
+   */
+  void insert(float i, size_t idx) {
+    assert(idx >= 0 && idx <= size_);
+    if (size_ == capacity_) {
+      expand();
+    }
+    if (!list[idx]) {
+      list[idx] = i;
+      return;
+    }
+    float *temp = new float[capacity_];
+    for (size_t j = 0; j < idx; j++) {
+      temp[j] = list[j];
+    }
+    temp[idx] = i;
+    for (size_t j = idx; j < size_; j++) {
+      temp[j + 1] = list[j];
+    }
+    size_++;
+    delete[] list;
+    list = temp;
+  }
+
+  /**
+   * @brief deletes the given object from array
+   *
+   * @param o object to be deleted
+   * @return Object removed object, now unlinked from array
+   */
+  float remove(size_t i) {
+    assert(i < size_ && i >= 0);
+    float *temp = new float[capacity_];
+    float cur;
+    cur = temp[i];
+    for (size_t j = 0; j < i; j++) {
+      // puts(list[j]->getValue());
+      temp[j] = list[j];
+    }
+    for (size_t j = i + 1; j < size_; j++) {
+      // puts(list[j]->getValue());
+      temp[j - 1] = list[j];
+    }
+    delete[] list;
+    list = temp;
+    size_--;
+    return cur;
+  }
+
+  void hash_me() {
+    hash_code = 0;
+    for (size_t i = 0; i < size_; i++) {
+      hash_code += (list[i] + 17) * 13;
+    }
+  }
+
+  size_t size() { return size_; }
+
+  size_t hash() {
+    if (hash_code == 0) {
+      hash_me();
+    }
+    return hash_code;
+  }
 };
--- CS4500-Assignment-2/part1/test-array.cpp
+++ CS4500-Assignment-2/part1/test-array.cpp
@@ -1,6 +1,6 @@
 #include "array.h"
-#include "int_array.h"
 #include "object.h"
+#include "string.h"
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,18 +9,14 @@
  * @brief Terminates program with error code
  *
  */
-void FAIL() {
-    exit(1);
-}
+void FAIL() { exit(1); }
 
 /**
  * @brief Terminates program with success message
  *
  * @param m Message to be outputted
  */
-void OK(const char* m) {
-    printf(m);
-}
+void OK(const char *m) { printf("test: %s passed\n", m); }
 
 /**
  * @brief Checks if condition true, fails otherwise
@@ -28,9 +24,9 @@ void OK(const char* m) {
  * @param p condition to be checked
  */
 void t_true(bool p) {
-    if (!p) {
-        FAIL();
-    }
+  if (!p) {
+    FAIL();
+  }
 }
 
 /**
@@ -39,38 +35,88 @@ void t_true(bool p) {
  * @param p condition to be checked
  */
 void t_false(bool p) {
-    if (p) {
-        FAIL();
-    }
+  if (p) {
+    FAIL();
+  }
 }
 
 void test_array1() {
-    Int_Array* array1 = new Int_Array();
-    t_true(array1->size() == 0);
-    int o1 = 10;
-    array1->append(o1);
-    t_true(array1->size() == 1);
-    t_true(array1->contains(o1));
-    array1->remove(0);
-    t_false(array1->contains(o1));
+  Int_Array *array1 = new Int_Array();
+  t_true(array1->size() == 0);
+  int o1 = 10;
+  array1->append(o1);
+  t_true(array1->size() == 1);
+  t_true(array1->contains(o1));
+  array1->remove(0);
+  t_false(array1->contains(o1));
 }
 
 void test_array2() {
-    Int_Array *array1 = new Int_Array();
-    int o1 = 10;
-    array1->append(o1);
-    int o2 = 20;
-    array1->insert(o2, 0);
-    t_true(o2 == array1->get(0));
-    array1->clear();
-    t_true(array1->size() == 0);
+  Int_Array *array1 = new Int_Array();
+  int o1 = 10;
+  array1->append(o1);
+  int o2 = 20;
+  array1->insert(o2, 0);
+  t_true(o2 == array1->get(0));
+  array1->clear();
+  t_true(array1->size() == 0);
 }
 
-int main()
-{
-    //test_object1();
-    test_array1();
-    test_array2();
-    OK("test1 passed\n");
-    return 0;
+/** everything in this block is created by implementor team*/
+void none_spec_test1() {
+  String *a = new String("Hellow");
+  Array *array = new Array();
+  t_true(array->size() == 0);
+  array->append(a);
+  t_true(array->size() == 1);
+
+  int i = 10;
+  Int_Array *int_array = new Int_Array();
+  int_array->append(i);
+  t_true(array->get(0) == 10);
+  OK("none_spec_test1");
+}
+
+void none_spec_test2() {
+  String *a = new String("Hellow");
+  String *b = new String("World");
+  String *c = new String("Xiaocheng");
+  String *d = new String("TianXia");
+  Array *array = new Array();
+  array->append(a);
+  array->append(b);
+  array->append(c);
+  array->append(d);
+  t_true(array->get(3)->equals(d));
+  t_true(array->get(0)->equals(a));
+  t_true(array->get(2)->equals(c));
+  OK("none_spec_test2");
+}
+
+void advance_none_spec_test() {
+  String *a = new String("Hellow");
+  String *b = new String("World");
+  String *c = new String("Xiaocheng");
+  String *d = new String("TianXia");
+  Array *array = new Array();
+  array->append(a);
+  array->append(b);
+  array->append(c);
+  array->append(d);
+  t_true(array->get(3)->equals(d));
+  t_true(array->get(0)->equals(a));
+  t_true(array->get(2)->equals(c));
+  array->remove(0);
+  t_true(array->get(0)->equals(b));
+  OK("advance_none_spec_test");
+}
+/** everything in this block is created by implementor team*/
+
+int main() {
+  test_array1();
+  test_array2();
+  none_spec_test1();
+  none_spec_test2();
+  advance_none_spec_test();
+  return 0;
 }
