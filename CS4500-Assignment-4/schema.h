027cb21057accb85f587482cd6d74d2151af9599
Xiaocheng-Zhang <zhang.xiaoch@husky.neu.edu>
2020-02-12 19:19:54 -0500

    old

--- CS4500-Assignment-4/schema.h
+++ CS4500-Assignment-4/schema.h
@@ -1,3 +1,11 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+#include "vec.h"
+
+#include "object.h"
+#include "string.h"
+
 /*************************************************************************
  * Schema::
  * A schema is a description of the contents of a data frame, the schema
@@ -7,50 +15,51 @@
  */
 class Schema : public Object {
  public:
- 
-  /** Copying constructor */
-  Schema(Schema& from)
- 
-  /** Create an empty schema **/
-  Schema()
- 
-  /** Create a schema from a string of types. A string that contains
+    
+
+   /** Copying constructor */
+   Schema(Schema &from) : Object() {}
+
+   /** Create an empty schema **/
+   Schema() : Object() {}
+
+   /** Create a schema from a string of types. A string that contains
     * characters other than those identifying the four type results in
     * undefined behavior. The argument is external, a nullptr argument is
     * undefined. **/
-  Schema(const char* types)
- 
-  /** Add a column of the given type and name (can be nullptr), name
-    * is external. Names are expectd to be unique, duplicates result
-    * in undefined behavior. */
-  void add_column(char typ, String* name)
- 
-  /** Add a row with a name (possibly nullptr), name is external.  Names are
-   *  expectd to be unique, duplicates result in undefined behavior. */
-  void add_row(String* name)
- 
-  /** Return name of row at idx; nullptr indicates no name. An idx >= width
-    * is undefined. */
-  String* row_name(size_t idx)
- 
-  /** Return name of column at idx; nullptr indicates no name given.
-    *  An idx >= width is undefined.*/
-  String* col_name(size_t idx)
- 
-  /** Return type of column at idx. An idx >= width is undefined. */
-  char col_type(size_t idx)
- 
-  /** Given a column name return its index, or -1. */
-  int col_idx(const char* name)
- 
-  /** Given a row name return its index, or -1. */
-  int row_idx(const char* name)
- 
-  /** The number of columns */
-  size_t width()
- 
-  /** The number of rows */
-  size_t length()
+   Schema(const char *types)
+
+       /** Add a column of the given type and name (can be nullptr), name
+        * is external. Names are expectd to be unique, duplicates result
+        * in undefined behavior. */
+       void add_column(char typ, String *name)
+
+       /** Add a row with a name (possibly nullptr), name is external.  Names
+        * are expectd to be unique, duplicates result in undefined behavior. */
+       void add_row(String *name)
+
+       /** Return name of row at idx; nullptr indicates no name. An idx >= width
+        * is undefined. */
+       String *row_name(size_t idx)
+
+       /** Return name of column at idx; nullptr indicates no name given.
+        *  An idx >= width is undefined.*/
+       String *col_name(size_t idx)
+
+       /** Return type of column at idx. An idx >= width is undefined. */
+       char col_type(size_t idx)
+
+       /** Given a column name return its index, or -1. */
+       int col_idx(const char *name)
+
+       /** Given a row name return its index, or -1. */
+       int row_idx(const char *name)
+
+       /** The number of columns */
+       size_t width()
+
+       /** The number of rows */
+       size_t length()
 };
  
 /*****************************************************************************
