84bc3cf7a358e37bde0b193b1da4b74b3493443a
Xiaocheng-Zhang <zhang.xiaoch@husky.neu.edu>
2020-02-13 21:17:01 -0500

    mew

--- CS4500-Assignment-4/schema.h
+++ CS4500-Assignment-4/schema.h
@@ -2,7 +2,6 @@
 #include "vec.h"
 #include <assert.h>
 
-
 /*************************************************************************
  * Schema::
  * A schema is a description of the contents of a data frame, the schema
@@ -12,15 +11,15 @@
  */
 class Schema : public Object {
 public:
-  Cvec *type_vec;
-  Svec *col_name_vec;
-  Svec *row_name_vec;
+  Vec *type_vec;
+  Vec *col_name_vec;
+  Vec *row_name_vec;
 
   /** Copying constructor */
   Schema(Schema &from) {
-    type_vec = from.type_vec;
-    col_name_vec = from.col_name_vec;
-    row_name_vec = from.row_name_vec;
+    type_vec = from.type_vec->copy();
+    col_name_vec = from.col_name_vec->copy();
+    row_name_vec = from.row_name_vec->copy();
   }
 
   /** Create an empty schema **/
@@ -43,18 +42,22 @@ public:
     col_name_vec = new Svec();
     row_name_vec = new Svec();
     for (size_t i = 0; i < strlen(types); i++) {
-      if (types[i] == 'I') {
-        type_vec->append('I');
-        col_name_vec->append(nullptr);
-      } else if (types[i] == 'B') {
-        type_vec->append('B');
-        col_name_vec->append(nullptr);
-      } else if (types[i] == 'F') {
-        type_vec->append('F');
-        col_name_vec->append(nullptr);
-      } else if (types[i] == 'S') {
-        type_vec->append('S');
-        col_name_vec->append(nullptr);
+      // if (types[i] == 'I') {
+      //   type_vec->append('I');
+      //   col_name_vec->append(nullptr);
+      // } else if (types[i] == 'B') {
+      //   type_vec->append('B');
+      //   col_name_vec->append(nullptr);
+      // } else if (types[i] == 'F') {
+      //   type_vec->append('F');
+      //   col_name_vec->append(nullptr);
+      // } else if (types[i] == 'S') {
+      //   type_vec->append('S');
+      //   col_name_vec->append(nullptr);
+      // }
+      if (type_check(types[i])) {
+        type_vec->append(types[i]);
+        col_name_vec->size_++;
       } else {
         std::cout << "The variable should not contain other than four "
                      "identifying type"
@@ -64,7 +67,9 @@ public:
     }
   }
 
-  bool type_check(char c) { return c == 'I' || c == 'B' || c == 'F' || c == 'S'; }
+  bool type_check(char c) {
+    return c == 'I' || c == 'B' || c == 'F' || c == 'S';
+  }
 
   /** Add a column of the given type and name (can be nullptr), name
    * is external. Names are expectd to be unique, duplicates result
@@ -88,27 +93,31 @@ public:
    * is undefined. */
   String *row_name(size_t idx) {
     assert(idx < row_name_vec->size_);
-    return row_name_vec->get(idx);
+    return row_name_vec->get_String(idx);
   }
 
   /** Return name of column at idx; nullptr indicates no name given.
    *  An idx >= width is undefined.*/
   String *col_name(size_t idx) {
     assert(idx < col_name_vec->size_);
-    return col_name_vec->get(idx);
+    return col_name_vec->get_String(idx);
   }
 
   /** Return type of column at idx. An idx >= width is undefined. */
   char col_type(size_t idx) {
     assert(idx < type_vec->size_);
-    return type_vec->get(idx);
+    return type_vec->get_char(idx);
   }
 
   /** Given a column name return its index, or -1. */
-  int col_idx(const char *name) { return col_name_vec->indexAt(new String(name)); }
+  int col_idx(const char *name) {
+    return col_name_vec->indexAt(new String(name));
+  }
 
   /** Given a row name return its index, or -1. */
-  int row_idx(const char *name) { return row_name_vec->indexAt(new String(name)); }
+  int row_idx(const char *name) {
+    return row_name_vec->indexAt(new String(name));
+  }
 
   /** The number of columns */
   size_t width() { return col_name_vec->size_; }
@@ -125,15 +134,17 @@ class Fielder : public Object {
 public:
   /** Called before visiting a row, the argument is the row offset in the
     dataframe. */
-  virtual void start(size_t r)
+  virtual void start(size_t r) {}
 
-      /** Called for fields of the argument's type with the value of the field.
-       */
-      virtual void accept(bool b) virtual void accept(
-          float f) virtual void accept(int i) virtual void accept(String *s)
+  /** Called for fields of the argument's type with the value of the field.
+   */
+  virtual void accept(bool b) {}
+  virtual void accept(float f) {}
+  virtual void accept(int i) {}
+  virtual void accept(String *s) {}
 
-      /** Called when all fields have been seen. */
-      virtual void done()
+  /** Called when all fields have been seen. */
+  virtual void done() {}
 };
 
 /*************************************************************************
@@ -147,37 +158,40 @@ public:
 class Row : public Object {
 public:
   /** Build a row following a schema. */
-  Row(Schema &scm)
+  Row(Schema &scm) {}
 
-      /** Setters: set the given column with the given value. Setting a column
-       * with a value of the wrong type is undefined. */
-      void set(size_t col, int val) void set(size_t col,
-                                             float val) void set(size_t col,
-                                                                 bool val)
-      /** The string is external. */
-      void set(size_t col, String *val)
+  /** Setters: set the given column with the given value. Setting a column
+   * with a value of the wrong type is undefined. */
+  void set(size_t col, int val);
+  void set(size_t col, float val);
+  void set(size_t col, bool val);
+  /** The string is external. */
+  void set(size_t col, String *val);
 
-      /** Set/get the index of this row (ie. its position in the dataframe. This
-       * is only used for informational purposes, unused otherwise */
-      void set_idx(size_t idx) size_t get_idx()
+  /** Set/get the index of this row (ie. its position in the dataframe. This
+   * is only used for informational purposes, unused otherwise */
+  void set_idx(size_t idx);
+  size_t get_idx();
 
-      /** Getters: get the value at the given column. If the column is not
-       * of the requested type, the result is undefined. */
-      int get_int(size_t col) bool get_bool(size_t col) float get_float(
-          size_t col) String *get_string(size_t col)
+  /** Getters: get the value at the given column. If the column is not
+   * of the requested type, the result is undefined. */
+  int get_int(size_t col);
+  bool get_bool(size_t col);
+  float get_float(size_t col);
+  String *get_string(size_t col);
 
-      /** Number of fields in the row. */
-      size_t width()
+  /** Number of fields in the row. */
+  size_t width();
 
-      /** Type of the field at the given position. An idx >= width is
-         undefined. */
-      char col_type(size_t idx)
+  /** Type of the field at the given position. An idx >= width is
+     undefined. */
+  char col_type(size_t idx);
 
-      /** Given a Fielder, visit every field of this row. The first argument is
-       * index of the row in the dataframe.
-       * Calling this method before the row's fields have been set is undefined.
-       */
-      void visit(size_t idx, Fielder &f)
+  /** Given a Fielder, visit every field of this row. The first argument is
+   * index of the row in the dataframe.
+   * Calling this method before the row's fields have been set is undefined.
+   */
+  void visit(size_t idx, Fielder &f);
 };
 
 /*******************************************************************************
@@ -192,11 +206,11 @@ public:
       should not be retained as it is likely going to be reused in the next
       call. The return value is used in filters to indicate that a row
       should be kept. */
-  virtual bool accept(Row &r)
+  virtual bool accept(Row &r);
 
-      /** Once traversal of the data frame is complete the rowers that were
-          split off will be joined.  There will be one join per split. The
-          original object will be the last to be called join on. The join method
-          is reponsible for cleaning up memory. */
-      void join_delete(Rower *other)
+  /** Once traversal of the data frame is complete the rowers that were
+      split off will be joined.  There will be one join per split. The
+      original object will be the last to be called join on. The join method
+      is reponsible for cleaning up memory. */
+  void join_delete(Rower *other);
 };
